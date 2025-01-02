6a95f5de8541376a88d9177bb9ff57cb69caa976
Ricky <rickyrizkiawan666@gmail.com>
2025-01-02 12:57:32 +0700

    Revert "finished part 1.3-1.5"
    This reverts commit 44d40e2025705c525cf9191d180a0bb73ef7eefc.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,32 +1,22 @@
 function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  };
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
   function Header({ course }) {
     return <h1>{course}</h1>;
   }
 
-  function Content({ parts }) {
+  function Content(props) {
     return (
       <>
-        {parts.map((part, index) => {
-          return <Part part={part.name} exercises={part.exercises} key={index} />;
-        })}
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
       </>
     );
   }
@@ -39,16 +29,22 @@ function App() {
     );
   }
 
-  function Total({ parts }) {
-    const TotalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-    return <p>Number of exercises {TotalExercises}</p>;
+  function Total(props) {
+    return <p>Number of exercises {props.exercises}</p>;
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 }

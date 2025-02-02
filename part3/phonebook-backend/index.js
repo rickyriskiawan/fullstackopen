b3c9e00e2bf2ed9568a467917abc823d1da4742c
Ricky <rickyrizkiawan666@gmail.com>
2025-02-02 22:28:07 +0700

    feat: api get data and save data, finished part 3.12-3.14

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,8 @@ let persons = [
   },
 ];
 
+const People = require('./models/people');
+
 app.use(cors());
 app.use(express.json());
 
@@ -39,8 +41,13 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons);
+app.get('/api/persons', async (req, res) => {
+  try {
+    const people = await People.find({});
+    res.status(200).json(people);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get('/info', (req, res) => {
@@ -49,14 +56,22 @@ app.get('/info', (req, res) => {
   res.send(`<p>phonebook has info ${personsLength} people </p> <br> ${date}`);
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
+app.get('/api/persons/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const person = await People.findById(id);
 
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
+    if (person) {
+      res.status(200).json(person);
+    } else {
+      res.status(404).json({
+        message: 'Data not found',
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
   }
 });
 
@@ -67,33 +82,33 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-app.post('/api/persons', (req, res) => {
-  const name = req.body.name;
-  const number = req.body.number;
+app.post('/api/persons', async (req, res) => {
+  try {
+    const name = req.body.name;
+    const number = req.body.number;
 
-  if (!name || !number) {
-    return res.status(400).json({
-      error: !name ? 'Name cant be empty' : 'Number cant be empty',
+    if (!name || !number) {
+      return res.status(400).json({
+        error: !name ? 'Name cant be empty' : 'Number cant be empty',
+      });
+    }
+
+    const newPerson = new People({
+      name: name,
+      number: number,
+    });
+
+    const person = await newPerson.save();
+
+    res.status(201).json({
+      message: 'Person Saved',
+      data: person,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server Error',
     });
   }
-
-  const existedPerson = persons.find((person) => person.name.toLowerCase() === name.toLowerCase());
-
-  if (existedPerson) {
-    return res.status(400).json({
-      error: 'Name must be unique',
-    });
-  }
-
-  const id = Math.floor(Math.random() * 1000);
-  const newPerson = {
-    id: id.toString(),
-    name: name,
-    number: number,
-  };
-  persons = persons.concat(newPerson);
-
-  res.json(newPerson);
 });
 
 app.listen(PORT, () => {
--- part3/phonebook-backend/models/people.js
+++ part3/phonebook-backend/models/people.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const paramater = process.argv;
 
 const uri = process.env.database_uri;
 
@@ -9,6 +10,14 @@ mongoose.connect(uri);
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 const Person = mongoose.model('Person', personSchema);

841c47d8c0f1985d519b8ec5e6f13a206d337f15
Ricky <rickyrizkiawan666@gmail.com>
2025-02-06 18:27:55 +0700

    add eslint

---
+++ part3/phonebook-backend/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+
+/** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+//   {languageOptions: { globals: globals.browser }},
+// ];
+
+// ...
+export default [
+  js.configs.recommended,
+
+  {
+    ignores: ['dist/**'],
+  },
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+    },
+
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+];

d9225bd2e83772a64d20d88456805e7cd24dee54
Ricky <rickyrizkiawan666@gmail.com>
2025-02-06 18:33:02 +0700

    change url database

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const URL = '/api/persons';
-const URL = 'http://localhost:3001/api/persons';
-// const URL = import.meta.env.VITE_URI;
+// const URL = 'http://localhost:3001/api/persons';
+const URL = import.meta.env.VITE_URI;
 
 async function getAll() {
   try {
